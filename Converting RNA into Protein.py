5b0582f35a21b18a49626b333b65bd922ee4e051
Anthony Federico <dephoona@gmail.com>
2016-05-07 21:22:18 -0400

    Update Converting RNA into Protein.py

--- Converting RNA into Protein.py
+++ Converting RNA into Protein.py
@@ -1,4 +1,26 @@
-#Text filed containing codon/protein pairs
+'''
+Text file containing codon/protein pairs (per line)
+For example...
+
+UUU F      CUU L      AUU I      GUU V
+UUC F      CUC L      AUC I      GUC V
+UUA L      CUA L      AUA I      GUA V
+UUG L      CUG L      AUG M      GUG V
+UCU S      CCU P      ACU T      GCU A
+UCC S      CCC P      ACC T      GCC A
+UCA S      CCA P      ACA T      GCA A
+UCG S      CCG P      ACG T      GCG A
+UAU Y      CAU H      AAU N      GAU D
+UAC Y      CAC H      AAC N      GAC D
+UAA Stop   CAA Q      AAA K      GAA E
+UAG Stop   CAG Q      AAG K      GAG E
+UGU C      CGU R      AGU S      GGU G
+UGC C      CGC R      AGC S      GGC G
+UGA Stop   CGA R      AGA R      GGA G
+UGG W      CGG R      AGG R      GGG G
+
+'''
+
 infile = open("codons.txt", "r")
 
 #Load Dictionary
@@ -21,3 +43,5 @@ while y < len(RNA):
     y += 3
 
 print Protein
+
+#Output looks something like this -> MAMAPRTEINSTRING Stop
