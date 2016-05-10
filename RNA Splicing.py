18cf3d4bab80e69ac998dfd8c12907ce603167a8
Anthony Federico <dephoona@gmail.com>
2016-05-10 19:03:30 -0400

    Update RNA Splicing.py

--- RNA Splicing.py
+++ RNA Splicing.py
@@ -3,21 +3,21 @@
 Text file containing codon/protein pairs (per line)
 For example...
 TTT F      CUU L      AUU I      GUU V
-UUC F      CUC L      AUC I      GUC V
-UUA L      CUA L      AUA I      GUA V
-UUG L      CUG L      AUG M      GUG V
-UCU S      CCU P      ACU T      GCU A
-UCC S      CCC P      ACC T      GCC A
-UCA S      CCA P      ACA T      GCA A
-UCG S      CCG P      ACG T      GCG A
-UAU Y      CAU H      AAU N      GAU D
-UAC Y      CAC H      AAC N      GAC D
-UAA Stop   CAA Q      AAA K      GAA E
-UAG Stop   CAG Q      AAG K      GAG E
-UGU C      CGU R      AGU S      GGU G
-UGC C      CGC R      AGC S      GGC G
-UGA Stop   CGA R      AGA R      GGA G
-UGG W      CGG R      AGG R      GGG G
+TTC F      CUC L      AUC I      GUC V
+TTA L      CUA L      AUA I      GUA V
+TTG L      CUG L      AUG M      GUG V
+TCT S      CCU P      ACU T      GCU A
+TCC S      CCC P      ACC T      GCC A
+TCA S      CCA P      ACA T      GCA A
+TCG S      CCG P      ACG T      GCG A
+TAT Y      CAU H      AAU N      GAU D
+TAC Y      CAC H      AAC N      GAC D
+TAA Stop   CAA Q      AAA K      GAA E
+TAG Stop   CAG Q      AAG K      GAG E
+TGT C      CGU R      AGU S      GGU G
+TGC C      CGC R      AGC S      GGC G
+TGA Stop   CGA R      AGA R      GGA G
+TGG W      CGG R      AGG R      GGG G
 
 Text file containing pre-RNA and splice sites
 >Pre-RNA_1
