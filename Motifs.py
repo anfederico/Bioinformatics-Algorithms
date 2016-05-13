053f5c31b37ef94ea41c6b5dc1a3463d2ed6ff20
Anthony Federico <dephoona@gmail.com>
2016-05-12 23:54:18 -0400

    Create Motifs.py

---
+++ Motifs.py
@@ -0,0 +1,80 @@
+infile = open("strings.txt", "r")
+
+temp_string = ""
+strings = []
+x = 0
+
+#Load strings from text file
+for string in infile:
+    if string[0] != ">":    
+        x = 1
+        temp_string = temp_string + string
+    if string[0] == ">":
+        if x == 1:
+            strings.append(temp_string)
+        temp_string = ""
+            
+
+#Remove midstring line breaks
+temp_strings = []
+for string in strings:
+    listing = list(string)
+    for pos in range(0,len(listing)-1):
+        if listing[pos] == "\n":
+            listing[pos] = ""
+        temp_string = "".join(listing)
+    temp_strings.append(temp_string)
+strings = temp_strings
+ref = strings[0]
+strings.remove(strings[0])
+
+
+#print "Strings:"
+#print strings
+#print "Reference:"
+#print ref
+#print "----------"
+#print
+
+all_motifs = []
+
+#Function for finding substrings between str and ref
+
+def find_substrings(str, ref):
+    motifs = []
+    match = 0
+    x = 0
+    y = 1
+    for j in range(0, 2):    
+        while y < len(str):
+            for i in range(0, len(str)):
+                if str[i:i+len(ref[x:y])] == ref[x:y]:
+                    match = 1
+            if match == 1:
+                if ref[x:y] != "\n":
+                    motifs.append(ref[x:y])
+                y += 1
+                match = 0
+            elif match == 0:
+                x += 1
+                y = x + 1
+    return motifs
+
+    
+for seq in strings:
+    all_motifs.append(find_substrings(seq, ref))
+
+common_motifs = []
+
+for substring in all_motifs[0]:
+    w = 0
+    for part in all_motifs:
+        w = 0
+        for sub in part:
+            if substring == sub:
+                w = 1
+    if w == 1:
+        common_motifs.append(substring)
+        
+print common_motifs
+print max(common_motifs, key = len)
