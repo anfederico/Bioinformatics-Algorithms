c14298aa464a6cdfcee30c7180065820ca84039a
Anthony Federico <dephoona@gmail.com>
2016-05-16 21:53:24 -0400

    Update and rename Dynamic Programing.py to Dynamic Programming.py

--- Dynamic Programming.py
+++ Dynamic Programming.py
@@ -1,11 +1,13 @@
 '''
+
 This program will return the total number of pairs of cats in a population remaining after the n-th month if all cats live for m months
 F[1] = 1 pair of baby cats (1 male + 1 female) 
 Each pair of cats reaches maturity in one month and produces a single pair of offspring (1 male + 1 female) each subsequent month including the month of death
+
 '''
 
-n = 99 
-m = 20
+n = 35
+m = 7
 
 F = {}
 F[0] = 0
@@ -39,3 +41,5 @@ for k in range (3,n+1):
     mort_fib(k)
 
 print F[n]
+
+#Output - > 5526636
