c3910ae03cd9a6daeba1646d9daee14ded627dc1
Anthony Federico <dephoona@gmail.com>
2016-05-20 21:32:12 -0400

    Update RNA Splicing.py

--- RNA Splicing.py
+++ RNA Splicing.py
@@ -12,11 +12,11 @@ TCA S      CCA P      ACA T      GCA A
 TCG S      CCG P      ACG T      GCG A
 TAT Y      CAT H      AAT N      GAT D
 TAC Y      CAC H      AAC N      GAC D
-TAA Stop   CAA Q      AAA K      GAA E
-TAG Stop   CAG Q      AAG K      GAG E
+TAA *      CAA Q      AAA K      GAA E
+TAG *      CAG Q      AAG K      GAG E
 TGT C      CGT R      AGT S      GGT G
 TGC C      CGC R      AGC S      GGC G
-TGA Stop   CGA R      AGA R      GGA G
+TGA *      CGA R      AGA R      GGA G
 TGG W      CGG R      AGG R      GGG G
 
 Text file containing pre-RNA and splice sites
@@ -101,6 +101,6 @@ while y < len(mRNA):
 
 print Protein
 
-#Output -> MVYIADKQHVASREAYGHMFKVCA
+#Output -> MVYIADKQHVASREAYGHMFKVCA*
 
 
