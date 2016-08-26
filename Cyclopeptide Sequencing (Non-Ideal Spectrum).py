53a50a7a1bd6bf6e455c9d576e67d1d90dc8b59e
Anthony Federico <dephoona@gmail.com>
2016-08-26 16:51:49 -0400

    Update Cyclopeptide Sequencing (Non-Ideal Spectrum).py

--- Cyclopeptide Sequencing (Non-Ideal Spectrum).py
+++ Cyclopeptide Sequencing (Non-Ideal Spectrum).py
@@ -3,257 +3,193 @@ import heapq
 import operator
 
 #Non-ideal experimental spectrum
-spec_strn = '456 783 658 508 156 361 426 1177 593 554 1137 245 1493 1390 1280 114 797 97 981 606 740 721 496 817 1232 753 583 1242 342 584 1293 1379 211 147 338 137 1356 1436 1048 867 469 1080 1076 359 996 512 887 527 1356 314 696 720 997 1248 1282 1248 1008 757 485 200 1362 261 103 1346 1024 1132 1396 622 773 892 676 772 559 622 415 626 1280 1037 417 626 270 114 1379 835 934 867 1122 497 398 1151 1394 910 710 1155 312 1095 939 360 137 813 1406 680 920 382 99 1078 1269 1181 371 114 900 871 284 852 245 964 1111 445 156 1209 871 1223 966 213 213 251 1269 1179 470 87 131 1134 1337 1133 529 715 573 57 601 1018 413 736 985 0 1067 224 1234 909 316 1337 1023 778 475 1379 259 641 356 224 '
+SpectrumNonIdealTemp = '456 783 658 508 156 361 426 1177 593 554 1137 245 1493 1390 1280 114 797 97 981 606 740 721 496 817 1232 753 583 1242 342 584 1293 1379 211 147 338 137 1356 1436 1048 867 469 1080 1076 359 996 512 887 527 1356 314 696 720 997 1248 1282 1248 1008 757 485 200 1362 261 103 1346 1024 1132 1396 622 773 892 676 772 559 622 415 626 1280 1037 417 626 270 114 1379 835 934 867 1122 497 398 1151 1394 910 710 1155 312 1095 939 360 137 813 1406 680 920 382 99 1078 1269 1181 371 114 900 871 284 852 245 964 1111 445 156 1209 871 1223 966 213 213 251 1269 1179 470 87 131 1134 1337 1133 529 715 573 57 601 1018 413 736 985 0 1067 224 1234 909 316 1337 1023 778 475 1379 259 641 356 224'
+SpectrumNonIdealTemp = SpectrumNonIdealTemp.split()
+SpectrumNonIdeal = []
+for number in SpectrumNonIdealTemp:
+    SpectrumNonIdeal.append(int(number))
+SpectrumNonIdeal = sorted(SpectrumNonIdeal)
 
-#Reorganize through listing
-spec_list = []
-spec_temp = ''
-for char in spec_strn:
-    if char != ' ':
-        spec_temp += char
-    else:
-        spec_list.append(int(spec_temp))
-        spec_temp = ''
-    
-sort_spec_list = deepcopy(sorted(spec_list))
+m = 18 #Number of high frequency peptides present in convolution    
+MinMass = 57 #Minimum mass of possible amino acid
+MaxMass = 200 #Maximum mass of possible amino acid
+Boundary = 5 #How many rounds in before Upper Score cutoff is enforced
+LowerScore = 3 #Lower Score cutoff
+UpperScore = 25 #Upper Score cutoff
 
-#============== FIND CONVOLUTION ==============#
-
-def find_convo(sort_spec_list, m):
-    
-    #Create matrix
+def FindConvolution(SpectrumNonIdeal, m, MinMass, MaxMass):
+    #Find convolution of the peptide
     matrix = []
-    for number in sort_spec_list:
+    for number in SpectrumNonIdeal:
         matrix.append(number)
-        matrix.append([0]*len(sort_spec_list))
-    
-    #Traverse and fill
+        matrix.append([0]*len(SpectrumNonIdeal))
     k = 1  
     i = 3
-    while i < len(sort_spec_list)*2:
+    while i < len(SpectrumNonIdeal)*2:
         j = 0
         k += 1
         while j < k-1:
-            matrix[i][j] = matrix[i-1]-sort_spec_list[j]
+            matrix[i][j] = matrix[i-1]-SpectrumNonIdeal[j]
             j += 1
         i += 2
-    
-    #Specify elements
     elements = []
     i = 3
-    while i < len(sort_spec_list)*2:
+    while i < len(SpectrumNonIdeal)*2:
         for element in matrix[i]:
             if element != 0:
                 elements.append(element)
         i += 2    
-    
-    #Element frequency
-    multi = {}
+    frequency = {}
     for element in sorted(elements):
-        if element >= 57 and element <= 200:
-            multi.setdefault(element,0)
-            multi[element] += 1
-
+        if element >= MinMass and element <= MaxMass:
+            frequency.setdefault(element,0)
+            frequency[element] += 1
     numbers = []
-    for element in multi:
-        numbers.append(multi[element]) 
+    for element in frequency:
+        numbers.append(frequency[element]) 
     minimum = min(heapq.nlargest(m, numbers))
-    
     output = []
-    for element in multi:    
-        if multi[element] >= minimum:
+    for element in frequency:    
+        if frequency[element] >= minimum:
             output.append(str(element))
-    
     return output
-    
-m = 18 #Number of high frequency peptides present in convolution
-freq_eles = find_convo(sort_spec_list, m)
 
-#Give each mass an arbitrary ASCII character
-lookup_num2char= {}
-lookup_char2num = {}
-i = 65
-for element in freq_eles:
-    if i == 91:
-        i = 97
-    lookup_num2char[element] = chr(i)
-    lookup_char2num[chr(i)] = element
-    i += 1
-
-sort_spec_list_temp = []
-for char in sort_spec_list:
-    sort_spec_list_temp.append(str(char))
-sort_spec_list = deepcopy(sort_spec_list_temp)
-
-#============== FIND LINEAR SPECTRUM ==============#
-
-def lin_spec(protein):
-    proteins = []
+def LinearSpectrum(peptide, CharToNum):
+    #Find the linear spectrum of the peptide
+    peptides = []
     sizes = ['0']
-    proteins.append(protein)
-    
-    n = len(protein)
-    
+    peptides.append(peptide)
+    n = len(peptide)
     for window in range(1,n):
         for i in range(0,n-window+1):
-            proteins.append(protein[i:i+window])
-        
-    for string in proteins:
+            peptides.append(peptide[i:i+window])
+    for string in peptides:
         total = 0
-        for amino_acid in string:
-            total += int(lookup_char2num[amino_acid])
+        for aminoacid in string:
+            total += int(CharToNum[aminoacid])
         sizes.append(str(total))
-    
     return sizes
-    
-#============== FIND CYCLO SPECTRUM ==============#
 
-def cyclo_spec(protein):
-    proteins2 = []
-    sizes2 = ['0']
-    proteins2.append(protein)
-    cyclic_protein = protein*2
-    
-    n = len(protein)
-    
+def CycloSpectrum(peptide, CharToNum):
+    #Find the cyclo spectrum of the peptide
+    peptides = []
+    sizes = ['0']
+    peptides.append(peptide)
+    cyclic_protein = peptide*2
+    n = len(peptide)
     for window in range(1,n):
         for i in range(0,n):
-            proteins2.append(cyclic_protein[i:i+window])
-        
-    for string in proteins2:
+            peptides.append(cyclic_protein[i:i+window])
+    for string in peptides:
         total = 0
-        for amino_acid in string:
-            total += int(lookup_char2num[amino_acid])
-        sizes2.append(str(total))
-        
-    return sizes2
-    
-#==================== FIND MASS ====================#
+        for aminoacid in string:
+            total += int(CharToNum[aminoacid])
+        sizes.append(str(total))
+    return sizes
 
-def find_mass(spectrum):
-    temp_list = []
-    for str in spectrum:
-        temp_list.append(int(str))
-    temp_list = sorted(temp_list)
-    return temp_list[len(temp_list)-1]
+def FindMass(spectrum):
+    temp = []
+    for num in spectrum:
+        temp.append(int(num))
+    temp = sorted(temp)
+    return temp[len(temp)-1]
 
-#================= LINEAR SCORING =================#
-
-def score(expe, spec):
-    temp_spec = deepcopy(spec)
+def Score(Experimental, Reference):
+    temp = deepcopy(Reference)
     score = 0
-    for exp_aa in expe:
+    for Expaa in Experimental:
         present = 0
-        for spec_aa in temp_spec:
-            if exp_aa == spec_aa:
+        for Refaa in temp:
+            if Expaa == Refaa:
                 present = 1
         if present == 1:
             score += 1
-            temp_spec.remove(exp_aa)
+            temp.remove(Expaa)
     return score 
 
-#===================== BEGIN ====================#
-
-#Correct mass to check for
-corr_mass = find_mass(sort_spec_list)
-
-combos = [] #Store potential peptides
-for amino_acid in lookup_char2num:
-    combos.append(amino_acid)
+def NonIdealSequencing(SpectrumNonIdeal, m,  MinMass, MaxMass, Boundary, LowerScore, UpperScore):
+    #Give each mass an arbitrary ASCII character
+    Frequency = FindConvolution(SpectrumNonIdeal, m, MinMass, MaxMass)
+    NumToChar= {}
+    CharToNum = {}
+    i = 65
+    for element in Frequency:
+        if i == 91:
+            i = 97
+        NumToChar[element] = chr(i)
+        CharToNum[chr(i)] = element
+        i += 1
     
-#Recursive method for building and scoring peptides
-leaders = {} #Store lead scoring peptides
-i = 0
-rounds = 1
-while len(combos) != 0:
-    
-    #Expand and score
-    temp_combos = {}
-    scores = []
-    for string in combos:
-        for amino_acid in lookup_char2num:     
+    #Convert integers to strings
+    temp = []
+    for char in SpectrumNonIdeal:
+        temp.append(str(char))
+    SpectrumNonIdeal = deepcopy(temp)
 
-            new_combo = string+amino_acid
-            new_spec = lin_spec(new_combo)
-            new_score = score(new_spec, sort_spec_list)            
-                
-            if find_mass(new_spec) < corr_mass:
-                
-                    scores.append(new_score)
-                    temp_combos[new_combo] = new_score
-            
-            elif find_mass(new_spec) == corr_mass:
-                leaders[new_combo] = score(cyclo_spec(new_combo), sort_spec_list)
-                
+    #Find correct mass of the non-ideal spectrum
+    CorrectMass = FindMass(SpectrumNonIdeal)
     
-    if len(scores) == 0:
-        break
-    
-    #Trim and push forward   
-    if rounds >= 5:
-        maxi = deepcopy(min(heapq.nlargest(3, scores)))
-    if rounds < 5:
-        maxi = deepcopy(min(heapq.nlargest(25, scores)))
-
-    combos = []
-    counter = 0
-    for string in temp_combos:
+    combos = [] #Store potential peptides
+    leaders = {} #Store lead scoring peptides
+    for aminoacid in CharToNum:
+        combos.append(aminoacid)
         
-        if temp_combos[string] >= maxi:
-
-            combos.append(string)
-            counter += 1
+    #Recursive method for building and scoring peptides
+    rounds = 1
+    while len(combos) != 0:
+        #Expand and score
+        CombosTemp = {}
+        Scores = []
+        for string in combos:
+            for aminoacid in CharToNum:     
+                NewCombo = string+aminoacid
+                NewSpec = LinearSpectrum(NewCombo, CharToNum)
+                NewScore = Score(NewSpec, SpectrumNonIdeal)            
+                
+                #If mass is too small, retry with another amino acid appended next round    
+                if FindMass(NewSpec) < CorrectMass:
+                        Scores.append(NewScore)
+                        CombosTemp[NewCombo] = NewScore
+                
+                #If mass is correct, score it
+                elif FindMass(NewSpec) == CorrectMass:
+                    leaders[NewCombo] = Score(CycloSpectrum(NewCombo, CharToNum), SpectrumNonIdeal)
+                    
+        if len(Scores) == 0:
+            break
         
-        if rounds >= 5:
-            if counter == 100:
-                break
-    rounds += 1
+        #Trim and push forward   
+        if rounds >= Boundary:
+            maximum = deepcopy(min(heapq.nlargest(LowerScore, Scores)))
+        if rounds < Boundary:
+            maximum = deepcopy(min(heapq.nlargest(UpperScore, Scores)))
+        combos = []
+        counter = 0
+        for string in CombosTemp:
+            if CombosTemp[string] >= maximum:
+                combos.append(string)
+                counter += 1
+            if rounds >= Boundary:
+                if counter == 100: #Limit recursion
+                    break
+        rounds += 1
+    
+    #Best scoring peptides with correct mass
+    LeadingScores = []
+    for protein in leaders:
+        LeadingScores.append(leaders[protein])
+    
+    #Select best of the best
+    TopScores = []    
+    for protein in leaders:
+        if leaders[protein] == max(LeadingScores):
+            string = ''
+            for amino_acid in protein:
+                string += CharToNum[amino_acid] + '-'
+            TopScores.append(string.strip('-'))
+    return TopScores    
 
-#Best scoring peptides with correct mass
-max_scores = []
-for protein in leaders:
-    max_scores.append(leaders[protein])
+Peptides = NonIdealSequencing(SpectrumNonIdeal, m, MinMass, MaxMass, Boundary, LowerScore, UpperScore)
 
-#Select best of the best
-tops = []    
-for protein in leaders:
-    if leaders[protein] == max(max_scores):
-        string = ''
-        for amino_acid in protein:
-            string += lookup_char2num[amino_acid] + '-'
-        tops.append(string.strip('-'))
-            
-for top in tops:
-    print top
-
-'''
-Output
-114-131-114-137-87-137-147-114-99-57-156-103-97
-156-103-97-114-131-114-137-87-137-147-114-99-57
-147-114-99-57-156-103-97-114-131-114-137-87-137
-114-97-103-156-57-99-114-147-137-87-137-114-74-57
-131-114-97-103-156-57-99-114-147-137-87-137-114
-137-87-137-147-114-99-57-156-103-97-114-131-114
-97-114-131-114-137-87-137-147-114-99-57-156-103
-87-137-147-114-99-57-156-103-97-114-131-114-137
-87-137-114-131-114-97-103-156-57-99-114-147-137
-99-57-156-103-97-114-131-114-137-87-137-147-114
-97-103-156-57-99-114-147-137-87-137-114-131-114
-57-99-114-147-137-87-137-114-131-114-97-103-99-57
-114-99-57-156-103-97-114-131-114-137-87-137-147
-137-147-114-99-57-156-103-97-114-131-114-137-87
-114-147-137-87-137-114-131-114-97-103-156-57-99
-156-57-99-114-147-137-87-137-114-131-114-97-103
-137-87-137-114-131-114-97-103-156-57-99-114-147
-147-137-87-137-114-131-114-97-103-156-57-99-114
-137-114-131-114-97-103-156-57-99-114-147-137-87
-103-156-57-99-114-147-137-87-137-114-131-114-97
-114-137-87-137-147-114-99-57-156-103-97-114-131
-99-114-147-137-87-137-114-131-114-97-103-156-57
-57-99-114-147-137-87-137-114-131-114-97-103-156
-131-114-137-87-137-147-114-99-57-156-103-97-114
-103-97-114-131-114-137-87-137-147-114-99-57-156
-114-131-114-97-103-156-57-99-114-147-137-87-137
-114-97-103-156-57-99-114-147-137-87-137-114-131
-57-156-103-97-114-131-114-137-87-137-147-114-99
-'''
+for peptide in Peptides:
+        print peptide
